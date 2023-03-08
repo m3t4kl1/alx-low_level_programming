e80907056a1fd39b19a7bd047d8c9e1a01432838
Aymane Metkali <metkaliaymane@gmail.com>
2023-03-08 07:14:19 -0800

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * help - helps sqrt function find sqrt
+ * @n: needs to be sqrt
+ * @x: sqrt of n
+ * Return: natural sqrt of n
+ */
+
+int help(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+	else if (x * x < n)
+		return (help(n, ++x));
+	return (-1);
+}
